b77be04927fc2f3e33b6dc59c9e7bfc4abec62dd
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:09:55 +0900

    published

--- js/jquery.getDate.js
+++ js/jquery.getDate.js
@@ -2,31 +2,22 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function($) {
-		'use strict';
+(function($) {
+	'use strict';
 
-		//제이쿼리가 함수일 때
-		if(typeof $ === 'function') {
-			var $get = $.get;
+	var $get = $.get;
 
-			/**
-			 * @name getDate
-			 * @since 2018-12-14
-			 * @return {date}
-			 */
-			window.getDate = function(callback) {
-				//함수일 때
-				if(typeof callback === 'function') {
-					$get('/jquery.getDate.txt', function(data, textStatus, jqXHR) {
-						callback(new Date(jqXHR.getResponseHeader('Date')));
-					});
-				}
-			};
-		}else{
-			throw '제이쿼리가 없습니다.';
+	/**
+	 * @name getDate
+	 * @since 2018-12-14
+	 * @param {function} callback {date}
+	 */
+	window.getDate = function(callback) {
+		//함수일 때
+		if(typeof callback === 'function') {
+			$get('/jquery.getDate.txt', function(data, textStatus, jqXHR) {
+				callback(new Date(jqXHR.getResponseHeader('Date')));
+			});
 		}
-	})(window.jQuery);
-}catch(e) {
-	console.error(e);
-}
+	};
+})(jQuery);
